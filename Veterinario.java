b0d4091cafee9532ce64453e20a39e3a1884fc41
ado753 <adonay.ramos@ceis.cs.umss.edu.bo>
2018-02-05 00:02:33 -0400

    aun me falta la parte de herencias

---
+++ Veterinario.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Veterinario {
+    AnimalDomestico animalDomestico;
+
+    public Veterinario(AnimalDomestico animalDomestico) {
+        this.animalDomestico = animalDomestico;
+    }
+
+    public void curar() {
+        if (this.animalDomestico instanceof Gato) {
+            System.out.println("se esta curando al gato");
+        } else
+            System.out.println("se esta curando al perro");
+        this.animalDomestico.saludar();
+
+    }
+}
